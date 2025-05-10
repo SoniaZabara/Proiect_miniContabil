c91f0572236424e3b775c3ebf7ae0f097b87e3ab
ioana-and30 <157801961+ioana-and30@users.noreply.github.com>
2025-05-10 17:55:47 +0300

    Tab Salarii

--- miniContabil/tabSalariiForm.Designer.cs
+++ miniContabil/tabSalariiForm.Designer.cs
@@ -95,11 +95,11 @@
             this.Departament});
             this.dataGridView1.Location = new System.Drawing.Point(6, 21);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(856, 214);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
--- miniContabil/tabSalariiForm.cs
+++ miniContabil/tabSalariiForm.cs
@@ -12,6 +12,7 @@ namespace miniContabil
 {
     public partial class tabSalariiForm : Form
     {
+        private int _index;
         public tabSalariiForm()
         {
             InitializeComponent();
@@ -32,6 +33,20 @@ namespace miniContabil
             dataGridView1.Rows[n].Cells[2].Value = textBoxFunctie.Text;
             dataGridView1.Rows[n].Cells[3].Value = textBoxSalariuBrut.Text;
             dataGridView1.Rows[n].Cells[4].Value = textBoxDepartament.Text;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            _index = e.RowIndex;
+
+            DataGridViewRow row = dataGridView1.Rows[_index];
+
+            textBoxId.Text = row.Cells[0].Value.ToString();
+            textBoxNumeAngajat.Text = row.Cells[1].Value.ToString();
+            textBoxFunctie.Text = row.Cells[2].Value.ToString();
+            textBoxSalariuBrut.Text = row.Cells[3].Value.ToString();
+            textBoxDepartament.Text = row.Cells[4].Value.ToString();
+
         }
     }
 }

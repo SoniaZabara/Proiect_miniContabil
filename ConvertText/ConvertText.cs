24e88138191f4300a14ba5fc469f647f2d10e600
SoniaZabara <sonia.zabara@student.tuiasi.ro>
2025-05-20 09:45:54 +0300

    Adaugare DLL-uri

---
+++ ConvertText/ConvertText.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertText
+{
+    public class ConvertText
+    {
+        public static decimal ConvertToDecimal(string input)
+        {
+            try
+            {
+                return decimal.Parse(input);
+            }
+            catch (FormatException)
+            {
+                return 0;
+            }
+            catch (OverflowException)
+            {
+                return 0;
+            }
+        }
+
+        public static int ConvertToInt(string input)
+        {
+            try
+            {
+                return int.Parse(input);
+            }
+            catch (FormatException)
+            {
+                return 0;
+            }
+            catch (OverflowException)
+            {
+                return 0;
+            }
+        }
+    }
+}
--- miniContabil/tab2Form.cs
+++ miniContabil/tab2Form.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using FirmUtil;
 using CostulProductieProfitDDL;
 
 namespace miniContabil
